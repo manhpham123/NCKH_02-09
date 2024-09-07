7822826504b13c0c4103934e17f6c296def9fbff
amatuercoder312 <lam312abc@gmail.com>
2024-09-07 10:15:39 +0700

    manh-lan4

--- front_end/src/pages/CheckFile/CheckFileTable/index.tsx
+++ front_end/src/pages/CheckFile/CheckFileTable/index.tsx
@@ -16,6 +16,7 @@ const CheckFileTable: FC = () => {
   });
   //const {data, mutate,isLoading} = useCheckFile(params);
   const isLoading = false;
+  // dữ liệu trả về, sắp xếp theo thời gian giảm dần
   const data = {
     "data": [
       {
@@ -23,70 +24,80 @@ const CheckFileTable: FC = () => {
         "md5": "f46bad29a32c8f0b27de63af58f76118",
         "sha1": "ee9051de481b6eb24bc625c2028f79b947035495",
         "sha256": "0e37b1329d6b6544ef24d204f4146f1445b5cdc33c46447de8e6c2c525f1fd6a",
-        "size":76
+        "size":76,
+        "timestamp": "2024-09-01 13:51:53"
       },
       {
         "filename": "example2.exe",
         "md5": "f0942ad9e7216ade8fd1118d33ec6151",
         "sha1": "90d805f00a2c27ca97dfdbd8965f082a6cd06af4",
         "sha256": "0da8a8a5eadc4fcb63a1ac452d084a8166882b3a9dbe202c574c632a194e50b2",
-        "size":84
+        "size":84,
+        "timestamp": "2024-09-01 13:51:53"
       },
       {
         "filename": "demo1.txt",
         "md5": "aa46425b529620edf837ea951cc28dcc",
         "sha1": "f0cf7442560e1c76ce61365a8b2f1f9dbb90dcd7",
         "sha256": "490d00c18b3bbfde087655b955c3ac3a32d36ec5fb8ce2d485ecd34ccce4d1db",
-        "size":121
+        "size":121,
+        "timestamp": "2024-09-01 13:51:53"
       },
       {
         "filename": "ddos.exe",
         "md5": "1217746c141eec70a23f191cfbcaff8d",
         "sha1": "26ddf97b32709c4ee87cf7248e906e24c6a68e77",
         "sha256": "bfb8861c57f4bb54628020ff0e95f7d000579f8c3a323bd1d3e6e4a872f3d245",
-        "size":50
+        "size":50,
+        "timestamp": "2024-09-01 13:51:53"
       },
       {
         "filename": "portscan.exe",
         "md5": "4d0bd356ea616ffa0687437edd9771da",
         "sha1": "30875ab155cde70844208e3af1cc6db16458e4b4",
         "sha256": "dcee09f8a5fe56552dd43d2400990ab65fde4821b78c87748417f8d5b904889f",
-        "size":300
+        "size":300,
+        "timestamp": "2024-09-01 13:51:53"
       },
       {
         "filename": "example3.exe",
         "md5": "5780e00e9cc633815e46f76ccb662cbe",
         "sha1": "da2e35e0e5ee30efbb51e95afa2be326e0211b18",
         "sha256": "2aec68c3818dc471865d8ca5f88d783943c1efff0f66360736a09026565035b4",
-        "size":101
+        "size":101,
+        "timestamp": "2024-09-01 13:51:53"
       },
       {
         "filename": "example4.txt",
         "md5": "7277b2e269528fb20d6d2681be354b93",
         "sha1": "7b4fc67e2938ae5c707162e2f06521bfc1719b23",
         "sha256": "cada223faa617fb038a5d6040d6bbe318a8d6a455377fbc5362ad82f0b741e2e",
-        "size":120
+        "size":120,
+        "timestamp": "2024-09-01 13:51:53"
       },
       {
         "filename": "demo2.exe",
         "md5": "1ffdb66e829387a9d10032af888e9639",
         "sha1": "06a5b4e196a197e5fce24fd07d374b5f0841ce2a",
         "sha256": "4931e5d3811a460f47678631f4bdc82c9c6e9176ae3940403e690bc82e3714a9",
-        "size":374
+        "size":374,
+        "timestamp": "2024-09-01 13:51:53"
       },
       {
         "filename": "demo3.txt",
         "md5": "0da9851aec9b55401560a80652ef22a6",
         "sha1": "f4614e579ae7977d467dd23fdadd4184f729f671",
         "sha256": "0fd55b4277f417ddcf927bc94bff8b96415b9630dfcf3e8aac3e153dc015a4a9",
-        "size":225
+        "size":225,
+        "timestamp": "2024-09-01 13:51:53"
       },
       {
         "filename": "example5.txt",
         "md5": "99847fa8352b1477dbf752ee33ecea14",
         "sha1": "23a857a56d8cad76a3be365e144f179cfc6088ed",
         "sha256": "28cf97f72cdba738fe94806e047be6ed4b261c1ef1eceec6f4f30a511d914aa7",
-        "size":200
+        "size":200,
+        "timestamp": "2024-09-01 13:51:53"
       }
     ],
     "limit": 10,
@@ -133,8 +144,8 @@ const CheckFileTable: FC = () => {
     },
     {
       key: 2,
-      title: "Tên File",
-      dataIndex: "filename",
+      title: "Thời Gian",
+      dataIndex: "timestamp",
       align: "center",
       render: (group) => (
         <Tooltip title={group}>
@@ -144,6 +155,18 @@ const CheckFileTable: FC = () => {
     },
     {
       key: 3,
+      title: "Tên File",
+      dataIndex: "filename",
+      align: "center",
+      render: (group) => (
+        <Tooltip title={group}>
+          <div className="inline-text">{group}</div>
+        </Tooltip>
+      ),
+    },
+    /*
+    {
+      key: 4,
       title: "MD5",
       dataIndex: "md5",
       align: "center",
@@ -154,7 +177,7 @@ const CheckFileTable: FC = () => {
       ),
     },
     {
-      key: 4,
+      key: 5,
       title: "SHA1",
       dataIndex: "sha1",
       align: "center",
@@ -165,7 +188,7 @@ const CheckFileTable: FC = () => {
       ),
     },
     {
-      key: 5,
+      key: 6,
       title: "SHA256",
       dataIndex: "sha256",
       align: "center",
@@ -175,8 +198,9 @@ const CheckFileTable: FC = () => {
         </Tooltip>
       ),
     },
+    */
     {
-      key: 6,
+      key: 7,
       title: "Kích Thước (KB)",
       dataIndex: "size",
       align: "center",
@@ -187,7 +211,7 @@ const CheckFileTable: FC = () => {
       ),
     },
     {
-      key: 7,
+      key: 8,
       title: "Hành Động",
       align: "center",
       width: "20%",
